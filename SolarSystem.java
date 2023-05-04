c7aedc82ae460b442ced42eecf32a9ee77c69ece
Karina Korsakova <126201548+KarinaKorsakova@users.noreply.github.com>
2023-05-04 23:19:51 +0300

    Update SolarSystem.java
    Update

--- SolarSystem.java
+++ SolarSystem.java
@@ -1,12 +1,19 @@
 public enum SolarSystem {
-    MERCURY(1, 0, 24d),
-    VENUS(2, 3.8d, 60d),
-    EARTH (3, 5d, 63.7d),
-    MARS (4, 5.5d, 348.8d),
-    JUPITER (5, 55d, 710d),
-    SATURN (6, 74d, 601d),
-    URANUS (7, 287d, 265d),
-    NEPTUNE (8, 300d, 2475d);
+    MERCURY(1, 0.0, 50.0, 2439.5, null),
+    VENUS(2, 38.0, 42.0, 6050.0, MERCURY),
+    EARTH (3, 50.0, 63.0, 6378.0, VENUS),
+    MARS (4, 55.0, 72.0, 3488.0, EARTH),
+    JUPITER (5, 550.0, 80.0, 7130.0, MARS),
+    SATURN (6, 746.0, 100.0, 6010.0, JUPITER),
+    URANUS (7, 28770.0, 150.0, 2650.0, SATURN),
+    NEPTUNE (8, 30000.0, 200.0, 24750.0, URANUS);
+
+    private final Integer orderFromSun;
+    public final Double distanceFromThePreviousPlanet;
+    public final Double distanceToTheNextPlanet;
+    private final Double distanceToTheSun;
+    private final Double radius;
+    private final SolarSystem previousPlanet;
 
     SolarSystem(int orderFromSun, double distanceFromPreviousPlanet, double radius, SolarSystem previousPlanet, String nextPlanet){
         this.orderFromSun = orderFromSun;
